7711201a0842c80c8cbb37b4f3bc6daa8fc7bb2e
Martin Grześlowski <martin.grzeslowski@gmail.com>
2014-01-12 14:49:55 +0100

    Removed stupid test

--- strangectrl/src/test/java/pl/grzeslowski/strangectrl/cmd/KeyCommandTest.java
+++ strangectrl/src/test/java/pl/grzeslowski/strangectrl/cmd/KeyCommandTest.java
@@ -1,7 +1,6 @@
 package pl.grzeslowski.strangectrl.cmd;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -32,23 +31,6 @@ public class KeyCommandTest {
         verify(inputUtils).pressKey(key);
     }
 
-    @Test
-    public void not_push_button_not_pushed_yet() throws Exception {
-
-        // given
-        final Key key = new Key("D");
-        final InputUtils inputUtils = mock(InputUtils.class);
-        final KeyCommand command = new KeyCommand(key, inputUtils);
-        final GraphicsDevice dev = mock(GraphicsDevice.class);
-        final double value = 0.0f;
-
-        // when
-        command.execute(dev, value);
-
-        // then
-        verify(inputUtils, never()).pressKey(key);
-    }
-  
     
     @Test
     public void push_button_release_push() throws Exception {
