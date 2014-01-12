0619cbe4e424bc270d57c435eba3184fb7efd9b5
Martin Grześlowski <martin.grzeslowski@gmail.com>
2014-01-12 17:02:23 +0100

    Test for two buttons in 1 conf

--- strangectrl/src/test/java/pl/grzeslowski/strangectrl/cmd/CommandFactoryTest.java
+++ strangectrl/src/test/java/pl/grzeslowski/strangectrl/cmd/CommandFactoryTest.java
@@ -4,6 +4,7 @@ import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import java.util.Iterator;
 import java.util.Set;
 
 import org.junit.Test;
@@ -36,7 +37,7 @@ public class CommandFactoryTest {
         assertThat(commands).hasSize(size);
     }
 
-    @Test(expected=NullPointerException.class)
+    @Test(expected = NullPointerException.class)
     public void null_conf() throws Exception {
 
         // given
@@ -72,5 +73,47 @@ public class CommandFactoryTest {
         final KeyCommand keyCommand = (KeyCommand) iCommand;
         keyCommand.execute(null, 1.0f);
         verify(inputUtils).pressKey(Lists.newArrayList(key));
+    }
+
+    @Test
+    public void get_commands_two_buttons() throws Exception {
+
+        // given
+        final Key key1 = new Key("Q");
+        final Button button1 = new Button("A", key1);
+
+        final Key key2 = new Key("W");
+        final Button button2 = new Button("C", key1, key2);
+
+        final Configuration configuration = new Configuration(button1, button2);
+
+        final InputUtils inputUtils = mock(InputUtils.class);
+        final CommandFactory commandFactory = new CommandFactory(inputUtils);
+
+        // expected
+        final int size = 2;
+
+        // when
+        final Set<ICommand> commands = commandFactory
+                .getCommands(configuration);
+
+        // then
+        assertThat(commands).hasSize(size);
+        
+        final Iterator<ICommand> it = commands.iterator();
+        final ICommand iCommand1 = it.next();
+        final ICommand iCommand2 = it.next();
+
+        assertThat(iCommand1.getClass()).isEqualTo(KeyCommand.class);
+        assertThat(iCommand2.getClass()).isEqualTo(KeyCommand.class);
+
+        final KeyCommand keyCommand1 = (KeyCommand) iCommand1;
+        final KeyCommand keyCommand2 = (KeyCommand) iCommand2;
+
+        keyCommand1.execute(null, 1.0f);
+        keyCommand2.execute(null, 1.0f);
+
+        verify(inputUtils).pressKey(Lists.newArrayList(key1));
+        verify(inputUtils).pressKey(Lists.newArrayList(key1, key2));
     }
 }
