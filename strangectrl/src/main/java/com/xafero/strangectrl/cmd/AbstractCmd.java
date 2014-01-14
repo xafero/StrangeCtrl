64810fde3939f579a1f1a4ac6b36b252665aa092
Martin Grześlowski <martin.grzeslowski@gmail.com>
2014-01-15 00:23:12 +0100

    Removed old class

--- strangectrl/src/main/java/com/xafero/strangectrl/cmd/AbstractCmd.java
+++
@@ -1,9 +0,0 @@
-package com.xafero.strangectrl.cmd;
-
-public abstract class AbstractCmd implements ICommand {
-	protected String[] args;
-
-	public AbstractCmd(String rawArgs) {
-		this.args = rawArgs.split(" ");
-	}
-}
