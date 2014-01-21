019fc30683bab9bf344302ce526446e5c748e209
Martin Grześlowski <martin.grzeslowski@gmail.com>
2014-01-21 22:01:06 +0100

    Test for normal case

--- strangectrl/src/test/java/com/xafero/strangectrl/input/ControllerPollerTest.java
+++ strangectrl/src/test/java/com/xafero/strangectrl/input/ControllerPollerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import net.java.games.input.Controller;
+import net.java.games.input.EventQueue;
 
 import org.junit.Test;
 
@@ -32,4 +33,25 @@ public class ControllerPollerTest {
 		verify(refresher).controllerNotAvailable(controller);
 	}
 
+	@Test
+	public void controller_valid() throws Exception {
+
+		// given
+		final Controller controller = mock(Controller.class);
+		when(controller.poll()).thenReturn(true);
+		when(controller.getEventQueue()).thenReturn(new EventQueue(0));
+
+		final IControllerCallback callback = mock(IControllerCallback.class);
+		final ControllersRefresher refresher = mock(ControllersRefresher.class);
+
+		final ControllerPoller poller = spy(new ControllerPoller(refresher,
+				controller, callback));
+
+		// when
+		poller.run();
+
+		// then
+		verify(controller).poll();
+		verify(controller).getEventQueue();
+	}
 }
