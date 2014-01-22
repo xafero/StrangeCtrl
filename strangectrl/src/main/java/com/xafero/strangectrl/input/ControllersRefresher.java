3ed0a1cb90ad425926452cf8ab81ab2062483fc2
Martin Grześlowski <martin.grzeslowski@gmail.com>
2014-01-22 18:08:16 +0100

    Fixed problem with consuming resources while non controllers is connectes

--- strangectrl/src/main/java/com/xafero/strangectrl/input/ControllersRefresher.java
+++ strangectrl/src/main/java/com/xafero/strangectrl/input/ControllersRefresher.java
@@ -2,19 +2,33 @@ package com.xafero.strangectrl.input;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 
 import net.java.games.input.Controller;
 import net.java.games.input.Controller.Type;
 
 public class ControllersRefresher {
+	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory
+			.getLogger(ControllersRefresher.class);
+	private static final long MAX_TIMES_DELAY = 1000;
 	private final InputUtils inputUtils;
+	private final AtomicLong timesTried = new AtomicLong(0);
 
 	public ControllersRefresher(final InputUtils inputUtils) {
 		this.inputUtils = checkNotNull(inputUtils);
 	}
 
 	public Set<Controller> getController() {
-		return inputUtils.getControllers(Type.GAMEPAD);
+		if (timesTried.incrementAndGet() >= MAX_TIMES_DELAY) {
+			logger.info("Getting new controllers from InputUtils");
+
+			timesTried.set(0);
+			return inputUtils.getControllers(Type.GAMEPAD);
+		} else {
+			return new HashSet<>();
+		}
+
 	}
 }
